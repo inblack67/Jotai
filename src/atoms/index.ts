72c8fa755f7815a50f28297ceb8ae1e2e08bee38
Aman Bhardwaj <inblack>
2021-03-31 16:33:39 +0530

    "strict": true

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,7 +1,7 @@
 import { atom } from "jotai";
+import { IPost } from "../interfaces";
 
 export const todoAtom = atom<string>('default');
 export const queriedPostAtom = atom<string>('');
-export const postsAtom = atom(async () => await (await fetch('https://jsonplaceholder.typicode.com/posts')).json());
-// export const filteredPostAtom = atom(get => get(postsAtom));
-export const filteredPostAtom = atom(get => get(postsAtom).filter(post => post.title.includes(get(queriedPostAtom))));
+export const postsAtom = atom<IPost[]>(async () => await (await fetch('https://jsonplaceholder.typicode.com/posts')).json());
+export const filteredPostAtom = atom<IPost[]>(get => get(postsAtom).filter(post => post.title.includes(get(queriedPostAtom))));
